ab79a0dca46f85323bda62661963576573a42993
DonaldLee <1026619991@qq.com>
2021-05-08 17:17:39 +0800

    文章模型出现不能保存到数据库的问题

--- db/migrate/20210507090200_create_articles.rb
+++ db/migrate/20210507090200_create_articles.rb
@@ -6,6 +6,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
       t.float :score
       t.integer :section
       t.integer :thumbs
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
